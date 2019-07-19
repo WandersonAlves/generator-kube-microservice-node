641a00937fcb5c944aa614be098653beaa05c17c
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2019-07-19 16:31:07 -0300

    feat: add swagger support via swagger-express-ts package

--- app/templates/src/routes/index.ts
+++ app/templates/src/routes/index.ts
@@ -1,4 +1,6 @@
 import <%= entityName %> from './<%= entityName %>';
+import * as swagger from 'swagger-express-ts';
+import * as express from 'express';
 import HealthCheck from './HealthCheck';
 import server from '../shared/server';
 
@@ -8,6 +10,23 @@ export default {
    */
   initRoutes() {
     server.use('/<%= entityNameLowerCase %>', <%= entityName %>);
+    server.use('/<%= entityNameLowerCase %>/api-docs/swagger', express.static('swagger'));
+    server.use('/<%= entityNameLowerCase %>/docs', express.static('docs'));
+    server.use(
+      '/api-docs/swagger/assets',
+      express.static('node_modules/swagger-ui-dist'),
+    );
+    server.use(
+      swagger.express({
+        definition: {
+          info: {
+            title: '<%= projectName %>',
+            version: '0.0.1',
+          },
+          basePath: '/<% entityNameLowerCase %>',
+        },
+      }),
+    );
     server.use('/health', HealthCheck);
   }
 };
