1f068c6506258d04d6f174891c05fc4eede91e77
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2019-08-07 14:30:06 -0300

    feat: add remaining CRUD methods
    Closes #4

--- app/templates/src/entities/SomeEntity/EntityController.ts
+++ app/templates/src/entities/SomeEntity/EntityController.ts
@@ -1,15 +1,26 @@
-import { OK } from 'http-status-codes';
-import { Response } from 'express';
+import { <%= interfaceName %> } from './<%= interfaceName %>';
+import { OK, NO_CONTENT, CREATED } from 'http-status-codes';
+import { Response, Request } from 'express';
+import { ValidationChain } from 'express-validator/check';
 import { inject } from 'inversify';
 import {
   controller,
   httpGet,
+  httpPost,
+  httpPut,
+  httpDelete,
   response,
+  request,
+  requestParam,
+  requestBody,
 } from 'inversify-express-utils';
 
 import <%= serviceName %> from './<%= serviceName %>';
-import withException from '../../shared/decorators/withException';
 import REFERENCES from '../../config/inversify.references';
+import EntityNotFoundException from '../../shared/exceptions/EntityNotFoundException';
+import withException from '../../shared/decorators/withException';
+
+const _createEdit<%= entityName %>Validator: ValidationChain[] = [];
 
 @controller('/<%= entityNameLowerCase %>')
 export default class <%= controllerName %> {
@@ -21,5 +32,38 @@ export default class <%= controllerName %> {
   async get<%= entityName %>s(@response() res: Response) {
     const result = await this.<%= serviceInstanceName %>.find({});
     res.status(OK).send(result);
+  }
+
+  @httpGet('/:id')
+  @withException
+  async get<%= entityName %>(@response() res: Response, @requestParam('id') id: string) {
+    const result = await this.<%= serviceInstanceName %>.findById(id);
+    if (!result) {
+      throw new EntityNotFoundException({ id });
+    }
+    res.status(OK).send(result);
+  }
+
+  @httpDelete('/:id')
+  @withException
+  async delete<%= entityName %>(@response() res: Response, @requestParam('id') id: string) {
+    await this.<%= serviceInstanceName %>.delete(id);
+    res.status(NO_CONTENT).send();
+  }
+
+  @httpPost('/', ..._createEdit<%= entityName %>Validator)
+  @withException
+  async post<%= entityName %>(@request() req: Request, @response() res: Response, @requestBody() <%= entityNameLowerCase %>: <%= interfaceName %>) {
+    this.<%= serviceInstanceName %>.validateRequest(req);
+    const new<%= entityName %> = await this.<%= serviceInstanceName %>.insert(<%= entityNameLowerCase %>);
+    res.status(CREATED).send(new<%= entityName %>);
+  }
+
+  @httpPost('/', ..._createEdit<%= entityName %>Validator)
+  @withException
+  async put<%= entityName %>(@request() req: Request, @response() res: Response, @requestBody() <%= entityNameLowerCase %>: <%= interfaceName %>) {
+    this.<%= serviceInstanceName %>.validateRequest(req);
+    const updated<%= entityName %> = await this.<%= serviceInstanceName %>.update(<%= entityNameLowerCase %>);
+    res.status(CREATED).send(updated<%= entityName %>);
   }
 }
