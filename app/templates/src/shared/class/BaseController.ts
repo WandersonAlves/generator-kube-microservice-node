5633c1ea418ab40507dd9ddf9ba5d91de0e0b352
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2019-08-22 16:18:07 -0300

    feat: add missing tsdocs and count method

--- app/templates/src/shared/class/BaseController.ts
+++ app/templates/src/shared/class/BaseController.ts
@@ -11,9 +11,7 @@ import Connection from './Connection';
 import env from '../../config/env';
 import UnprocessableEntityException from '../exceptions/UnprocessableEntityException';
 
-type InterfaceBoolean<T> = {
-  [P in keyof T]?: boolean;
-};
+type InterfaceBoolean<T> = { [P in keyof T]?: boolean };
 
 @injectable()
 export class BaseController<Interface extends IMongoModel> implements DatabaseOperations<Interface> {
@@ -31,6 +29,7 @@ export class BaseController<Interface extends IMongoModel> implements DatabaseOp
   /**
    * Saves the new Mongoose Model
    * @param entity A object that matchs a mongoose schema
+   * @param databaseName Set this to query on another database in the current mongo connection
    */
   insert(entity: Interface, databaseName?: string): Promise<Interface> {
     const _model = this._getModel(databaseName);
@@ -40,6 +39,10 @@ export class BaseController<Interface extends IMongoModel> implements DatabaseOp
   /**
    * Finds a Document by ObjectId
    * @param id A ObjectId from Mongoose schema
+   * @param lean Sets the lean option.
+   * Documents returned from queries with the lean option enabled are plain javascript objects, not MongooseDocuments.
+   * They have no save method, getters/setters or other Mongoose magic applied.
+   * @param databaseName Set this to query on another database in the current mongo connection
    * @returns A Promise with a single Document
    */
   findById(id: string, lean: boolean = true, databaseName?: string): Promise<Interface> {
@@ -52,6 +55,10 @@ export class BaseController<Interface extends IMongoModel> implements DatabaseOp
    * @param params.filter Object used to filter Documents
    * @param params.pagination Object with skip, limit properties to control pagination
    * @param params.fieldsToShow Object containing the fields to return from the Documents
+   * @param lean Sets the lean option.
+   * Documents returned from queries with the lean option enabled are plain javascript objects, not MongooseDocuments.
+   * They have no save method, getters/setters or other Mongoose magic applied.
+   * @param databaseName Set this to query on another database in the current mongo connection
    * @returns A Promise with a Array of Documents found
    */
   find(
@@ -76,6 +83,10 @@ export class BaseController<Interface extends IMongoModel> implements DatabaseOp
    * @param params.filter Object used to filter Documents
    * @param params.pagination Object with skip, limit properties to control pagination
    * @param params.fieldsToShow Object containing the fields to return from the Documents
+   * @param lean Sets the lean option.
+   * Documents returned from queries with the lean option enabled are plain javascript objects, not MongooseDocuments.
+   * They have no save method, getters/setters or other Mongoose magic applied.
+   * @param databaseName Set this to query on another database in the current mongo connection
    * @returns A Promise with a single Document
    */
   findOne(
@@ -97,6 +108,10 @@ export class BaseController<Interface extends IMongoModel> implements DatabaseOp
   /**
    * Deletes a Mongoose Document
    * @param id A ObjectId from Mongoose schema
+   * @param lean Sets the lean option.
+   * Documents returned from queries with the lean option enabled are plain javascript objects, not MongooseDocuments.
+   * They have no save method, getters/setters or other Mongoose magic applied.
+   * @param databaseName Set this to query on another database in the current mongo connection
    */
   delete(id: string, lean: boolean = true, databaseName?: string): Promise<Interface> {
     const _model = this._getModel(databaseName);
@@ -105,6 +120,7 @@ export class BaseController<Interface extends IMongoModel> implements DatabaseOp
   /**
    * Updates a Document
    * @param params A object that matchs a mongoose schema with a currently know ObjectId
+   * @param databaseName Set this to query on another database in the current mongo connection
    */
   update(params: Interface, databaseName?: string): Promise<Interface> {
     const _model = this._getModel(databaseName);
@@ -113,10 +129,20 @@ export class BaseController<Interface extends IMongoModel> implements DatabaseOp
   /**
    * Save multiple documents
    * @param entities Array os objects to save
+   * @param databaseName Set this to query on another database in the current mongo connection
    */
   insertMany(entities: Interface[], databaseName?: string): Promise<Interface[]> {
     const _model = this._getModel(databaseName);
     return _model.insertMany(entities) as any;
+  }
+  /**
+   * Get the count of documents by a given filter
+   * @param filter Object used to filter Documents
+   * @param databaseName Set this to query on another database in the current mongo connection
+   */
+  count(filter: Partial<MongoMerger<Interface>>, databaseName?: string): Promise<number> {
+    const _model = this._getModel(databaseName);
+    return _model.count(filter) as any;
   }
   /**
    * Validates a request using `express-validator`
@@ -131,10 +157,10 @@ export class BaseController<Interface extends IMongoModel> implements DatabaseOp
     return;
   }
   /**
-   * Gets a model instance from a given database
+   * Gets a model instance from a given database on the current connection
    * @param databaseName database name
    */
-  private _getModel(databaseName: string = this._defaultDB): Model<Document, any> {
+  private _getModel(databaseName: string = this._defaultDB): Model<Document> {
     let _model = this._model;
     if (databaseName) {
       const conn = this._connection.useDB(databaseName);
