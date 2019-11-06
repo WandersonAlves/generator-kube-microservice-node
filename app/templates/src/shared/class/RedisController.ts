c263a86bd925aafaf7ba1d6215bf4bb712a9de2d
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2019-11-06 14:45:23 -0300

    feat(node): update redis controller

--- app/templates/src/shared/class/RedisController.ts
+++ app/templates/src/shared/class/RedisController.ts
@@ -1,46 +1,52 @@
 import { createClient, RedisClient } from 'redis';
+import { injectable } from 'inversify';
 import { promisify } from 'util';
 import env from '../../config/env';
-import { injectable } from 'inversify';
 
 @injectable()
 export default class RedisController {
-  private client: RedisClient;
+  client: RedisClient;
   private getAsync;
-  private setAsync;
 
   constructor() {
-    this.client = createClient({url: env.redis_url});
+    this.client = createClient({ url: env.redis_url });
     this.getAsync = promisify(this.client.get).bind(this.client);
-    this.setAsync = promisify(this.client.set).bind(this.client);
   }
-
-  getCache(key: string): Promise<any> {
+  /**
+   * Get a value from a key in redis using GET command
+   * @param key Key
+   */
+  get<T>(key: string): Promise<T> {
     return new Promise(async (resolve, reject) => {
       try {
         const result = await this.getAsync(key);
         if (result) {
-          resolve (JSON.parse(result));
-        }
-        else {
+          resolve(JSON.parse(result));
+        } else {
           resolve(null);
         }
-      }
-      catch (err) {
+      } catch (err) {
         reject(err);
       }
     });
   }
-
-  setCache(key: string, value: any, operation: string = 'EX', expireTimer = 15): Promise<any> {
-    return new Promise(async (resolve, reject) => {
-      try {
-        await this.setAsync(key, JSON.stringify(value), operation, expireTimer);
-        resolve(true);
-      }
-      catch (err) {
-        reject(err);
-      }
-    });
+  /**
+   * Set a key in redis using SET command
+   * @param key Key
+   * @param value Value
+   */
+  set(key: any, value: any) {
+    this.client.set(key, JSON.stringify(value));
+  }
+  /**
+   * Set a key in redis using SETEX command
+   *
+   * Useful to set a expire timer on key
+   * @param key Key
+   * @param value Value
+   * @param ttl Time To Live (in seconds)
+   */
+  setex(key: any, value: any, ttl: number) {
+    this.client.setex(key, ttl, JSON.stringify(value));
   }
 }
