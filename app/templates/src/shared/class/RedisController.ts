31c9916ae7526a100a65862cff832137c3476540
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2019-11-13 02:57:53 -0300

    feat(redis): add withRedis helper function

--- app/templates/src/shared/class/RedisController.ts
+++ app/templates/src/shared/class/RedisController.ts
@@ -6,11 +6,41 @@ import env from '../../config/env';
 @injectable()
 export default class RedisController {
   client: RedisClient;
-  private getAsync;
+  private getAsync: (key: string) => Promise<string>;
 
   constructor() {
     this.client = createClient({ url: env.redis_url });
     this.getAsync = promisify(this.client.get).bind(this.client);
+  }
+  /**
+   * Returns a promise that resolve with cache or concrete data from a given callback function
+   *
+   * Example
+   * ```typescript
+   * const redisKey = 'randomKey';
+   * const expireInSeconds = 1000; // 1seg TTL
+   * return res.status(OK).send(
+   *   await this.redis.withRedis({ key: redisKey, expires: expireInSeconds }, async () => {
+   *     return this.find({});
+   *   }),
+   * );
+   * ```
+   * @param params.key Redis Key
+   * @param params.expires TTL value for given key
+   * @param callback Function that returns something and save it on redis in case cache is empty
+   */
+  async withRedis<T>(params: { key: string; expires?: number }, callback: () => Promise<T>) {
+    const cache = await this.get(params.key);
+    if (!cache) {
+      const result = await callback();
+      if (params.expires) {
+        this.setex(params.key, result, params.expires);
+      } else {
+        this.set(params.key, result);
+      }
+      return result;
+    }
+    return cache;
   }
   /**
    * Get a value from a key in redis using GET command
