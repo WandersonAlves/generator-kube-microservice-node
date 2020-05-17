9ba4e3038cd2cf95092680fdd85dbbe2582f9cc6
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2020-05-17 04:45:56 -0300

    refactor: change thins on logger

--- app/templates/src/shared/utils/logger.ts
+++ app/templates/src/shared/utils/logger.ts
@@ -1,34 +1,35 @@
-import * as appRoot from 'app-root-path';
 import { createLogger, format, transports } from 'winston';
 const { printf, colorize } = format;
 const colorizer = colorize();
 
-const consoleFormatter = printf(({ level, message, label, timestamp }: { [key: string]: string }) => {
+const timestampFormatter = () =>
+  new Date().toLocaleString('pt-BR', {
+    timeZone: 'America/Sao_Paulo',
+  });
+
+const consoleFormatter = printf(({ stack, level, message, label, timestamp }: { [key: string]: string }) => {
   const levelLabelColorized = colorizer.colorize(level, `${level}:${label}`);
   const timestampColorized = `\u{1b}[90;1m${new Date(timestamp).toLocaleTimeString('pt-BR', { hour12: false })}\u{1b}[0m`;
-  return `${timestampColorized} ${levelLabelColorized}: ${message}`;
+  return `${timestampColorized} ${levelLabelColorized}: ${stack ? stack : message}`;
 });
 
-const fileFormatter = printf(({ level, message, label, timestamp }: { [key: string]: string }) => {
-  const hour = new Date(timestamp).toLocaleTimeString('pt-BR', { hour12: false });
-  return `${hour} ${level}:${label} - ${message}`;
-});
-
-export const logger = createLogger({
-  level: 'silly',
+const baseLogger = createLogger({
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
   defaultMeta: { label: 'main' },
-  format: format.combine(format.timestamp()),
-  transports: [
-    new transports.File({
-      level: 'info',
-      handleExceptions: true,
-      filename: `${appRoot}/logs/app.log`,
-      format: fileFormatter,
-      maxsize: 5242880, // 5MB
-      maxFiles: 5,
-    }),
-    new transports.Console({
-      format: consoleFormatter,
-    }),
-  ],
+  // @ts-ignore
+  format: format.combine(format.errors({ stack: true }), format.timestamp({ format: timestampFormatter })),
 });
+
+/**
+ * Returns a pretty represetation of a json for a give object
+ * @param obj An object
+ */
+export const jsonString = (obj: any) => `\n${JSON.stringify(obj, null, 2)}\n`;
+
+baseLogger.add(
+  new transports.Console({
+    format: consoleFormatter,
+  }),
+);
+
+export const logger = baseLogger;
